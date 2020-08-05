217949f259bbcc4d77a9124ca284d51b41dd9eb4
shimna <shimnaa.arangil@gmail.com>
2020-08-05 18:15:31 +0530

    Add review to products functionality

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,8 @@ app_name = 'products'
 urlpatterns = [
     path('', views.product_list, name='product-list'),
     path('category/<str:category_id>', views.product_list_category, name='prod-category'),
-    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
+    #path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
+    path('product/<str:pk>/', views.productdetail, name='product-detail'),
+    path('review/<str:product_id>', views.review, name='review'),
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Product, Category
+from .models import Product, Category, Review
 from django.views.generic import DetailView
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def product_list(request):
@@ -22,15 +24,49 @@ def product_list_category(request, category_id):
     }
     return render(request, 'Homepage.html', context)
 
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = 'product_detail.html'
+#class ProductDetailView(DetailView):
+#    model = Product
+#    template_name = 'product_detail.html'
+#
+#    def get_context_data(self, **kwargs):
+#        context = super(ProductDetailView, self).get_context_data(**kwargs)
+#        context['categories'] = Category.objects.all()
+#        return context
 
-    def get_context_data(self, **kwargs):
-        context = super(ProductDetailView, self).get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
-        return context
+def productdetail(request, pk):
+    categories = Category.objects.all()
+    product = Product.objects.get(id=pk)
+    review_qs = product.review_set.all()
+    context = {
+        'product': product,
+        'categories': categories,
+        'reviews': review_qs
+    }
+    return render(request, 'product_detail.html', context)
 
+
+def review(request, product_id):
+    if request.method == 'POST':
+        data = {
+            'user': request.user,
+            'rating': int(request.POST.get('rating')),
+            'comment': request.POST.get('notes')
+        }
+        try:
+
+            response = Review.objects.create(
+                user=request.user,
+                rating=data.get('rating'),
+                comment=data.get('comment'),
+                product=Product.objects.get(id=product_id)
+            )
+            print("added review")
+            messages.success(request, "New Review added")
+        except Exception as e:
+            print(e)
+            messages.warning(
+                request, 'Got an error when trying to add a new Review')
+    return redirect('/')
 
 
 
